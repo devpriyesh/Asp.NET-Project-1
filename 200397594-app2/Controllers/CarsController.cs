0ad70bbb76223052f91b8c5487e947149fea8a4a
devpriyesh <priyeshhingrajia2601@gmail.com>
2019-05-27 21:19:59 -0400

    added car list

---
+++ 200397594-app2/Controllers/CarsController.cs
@@ -0,0 +1,29 @@
+﻿using _200397594_app2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _200397594_app2.Controllers
+{
+    public class CarsController : Controller
+    {
+        List<Car> carList = new List<Car>()
+        {
+            new Car {Id=1 , Brand = "Toyota" , Model = "Corolla" , Type = "Comapact" , Year= 2017} ,
+            new Car {Id=1 , Brand = "Toyota" , Model = "Corolla" , Type = "Comapact" , Year= 2017} ,
+            new Car {Id=1 , Brand = "Toyota" , Model = "Corolla" , Type = "Comapact" , Year= 2017}
+        };
+        // GET: Cars
+        public ActionResult Index()
+        {
+            // return Content("/cars: This is the list of all the cars from Content()");
+            // return HttpNotFound();
+            return View(carList);
+            
+
+
+        }
+    }
+}
---
+++ 200397594-app2/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200397594_app2.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+
+    }
+
+}
